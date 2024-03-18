8fc6be2aacb75bb146322f76c4644d90a8ef726e
Yichi Zhang <zyc23451@outlook.com>
2024-03-18 09:44:15 +0000

    create modulized structure

---
+++ main.py
@@ -0,0 +1,8 @@
+"""!@mainpage Lighthouse Problem
+@brief This is the main page for the solutions for lighthouse problem
+
+@details This contains all the codes you need to reproduce all the figures and results in the report.
+@author Created by Yichi Zhang (yz870) on 18/03/2024
+"""
+
+# Import the necessary packages
